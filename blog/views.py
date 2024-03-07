e9d7a0868511c59f80ab2bcd79b28ba4edcb8df0
jose you <youjoseline@gmail.com>
2024-03-07 16:20:53 +0000

    Finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, "blog/index.html")
+    # fetch all the Post objects in the system
+    posts = Post.objects.filter()
+    return render(request, "blog/index.html", {"posts": posts})
+    
